1ba1efd4a614270fb14e21441ab6c3d2037a2cc5
Daniel Madaschi <nunean@gmail.com>
2021-03-03 20:34:25 -0300

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'Applicant', at: 'auth'
+
+  mount_devise_token_auth_for 'HeadHunter', at: 'auth'
+  as :head_hunter do
+    # Define routes for HeadHunter within this block.
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
